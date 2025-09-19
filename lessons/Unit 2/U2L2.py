835553445620be6c659b7cf297e860c3a26fcad9
753200NB <753200@pdsb.net>
2025-09-19 13:39:52 -0400

    Create U2L2.py

---
+++ lessons/Unit 2/U2L2.py
@@ -0,0 +1,13 @@
+inches = 10 ** 12  # one trillion
+(foot, inches) = divmod(inches, 12)
+(yard, foot_) = divmod(foot, 3)
+(miles, yard_) = divmod(yard, 1760)
+(tothemoondogecoin, miles_) = divmod(miles, 238855)
+
+
+print("One trillion inches is the same as going to the moon and back")
+print(tothemoondogecoin, "times plus;", miles_, " miles, ", yard_,
+      " yards, ", foot_, " feet; and ", inches, " inches")
+
+print()
+# part 3
